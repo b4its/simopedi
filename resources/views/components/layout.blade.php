a9b34e708997caac08733075bc63712f022074fd
HandyCodez <ahanug28@gmail.com>
2025-06-13 14:25:35 +0800

    add chartjs & leaflet

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,11 +9,24 @@
     {{-- BOOTSTRAP CDN --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-        {{-- TAILWIND CDN --}}
-        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    {{-- TAILWIND CDN --}}
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    {{-- LEAFTLET CSS --}}
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 </head>
 
 <body>
+
+    {{-- CHARTJS --}}
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.3.2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/luxon@1.27.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-luxon@1.0.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-streaming@2.0.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-streaming@2"></script>
+    {{-- LEAFLET --}}
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 
     {{ $slot }}
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,2 @@
 <x-layout>
-    <h1 class="text-3xl font-bold underline">
-      adawdas
-    </h1>
 </x-layout>
